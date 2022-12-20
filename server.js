3626f95d531340465f594a89413b44025508411d
swadesh kelkar <swadeshkelkar07@gmail.com>
2022-12-21 01:39:54 +0530

    fixed server

--- server.js
+++ server.js
@@ -35,13 +35,6 @@ router.post("/run", cors(), (req, res) => {
 
 app.use(router);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
 
 const port = process.env.PORT || 5000;
 
